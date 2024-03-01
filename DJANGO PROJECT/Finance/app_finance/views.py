c1e2e04d844707f13fbef54586cc8e32e0506765
eudivanmelo <eudivan44@gmail.com>
2024-03-01 20:10:38 -0300

    Iniciado página de login

--- DJANGO PROJECT/Finance/app_finance/views.py
+++ DJANGO PROJECT/Finance/app_finance/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.http import HttpRequest
 
 def home(request):
+    if not request.user.is_authenticated:
+        return redirect(f"{settings.LOGIN_URL}")
+    
     return render(request, 'home.html')
+
+def login(request: HttpRequest):
+    return render(request, 'login.html')
