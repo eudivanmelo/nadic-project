265f2b8084405e971ad6ce49da4779ebd3352137
eudivanmelo <eudivan44@gmail.com>
2024-03-16 15:24:35 -0300

    Página de login finalizada

--- DJANGO PROJECT/Finance/app_finance/views.py
+++ DJANGO PROJECT/Finance/app_finance/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.shortcuts import redirect, render
 from django.http import HttpRequest
+from django.contrib.auth import authenticate, login
 
 def home(request):
     if not request.user.is_authenticated:
@@ -8,5 +9,20 @@ def home(request):
     
     return render(request, 'home.html')
 
-def login(request: HttpRequest):
-    return render(request, 'login.html')
+def login_view(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirecione para a página de sucesso ou para qualquer página que desejar
+            return redirect('home')
+        else:
+            error_message = "Credenciais inválidas. Por favor, tente novamente."
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
